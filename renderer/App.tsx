51d57b46b691664ffa9d2847a2c9db8f8f095985
inside-hakumai <inside.rice164@gmail.com>
2021-12-12 16:41:36 +0900

    検索結果のを十字キー上下で選択できる挙動を実装

--- renderer/App.tsx
+++ renderer/App.tsx
@@ -1,29 +1,83 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import darcula from 'react-syntax-highlighter/dist/esm/styles/hljs/darcula'
 
+const sampleSuggestItems = [
+  'SpringBoot @Transactional',
+  'SpringBoot Mybatisのテスト',
+  'SpringBoot Thymeleafのテスト',
+  'WHERE句でnullを指定する',
+  'hoge',
+  'huga',
+]
+
+const sampleCode = 'SELECT * FROM <テーブル名> WHERE <カラム名> IS NULL;'
+
 const App: React.VFC = () => {
   const [isDirty, setIsDirty] = useState(false)
+  const [suggestItems, setSuggestItems] = useState<string[]>([])
+  const [selectedItem, setSelectedItem] = useState<number | null>(null)
 
-  const sampleCode = 'SELECT * FROM <テーブル名> WHERE <カラム名> IS NULL;'
+  const onFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsDirty(true)
+    if (event.target.value.length === 0) {
+      clearResult()
+    } else {
+      // TODO: ここに検索処理を書く
+
+      setSuggestItems(sampleSuggestItems)
+    }
+  }
+
+  const clearResult = () => {
+    setIsDirty(false)
+    setSuggestItems([])
+    setSelectedItem(null)
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+      if (
+        selectedItem === null ||
+        (event.key === 'ArrowDown' && selectedItem === suggestItems.length - 1) ||
+        (event.key === 'ArrowUp' && selectedItem === 0)
+      ) {
+        return
+      }
+
+      const nextIndex = selectedItem + (event.key === 'ArrowDown' ? 1 : -1)
+      setSelectedItem(nextIndex)
+      console.debug('Selected item:', nextIndex)
+    }
+  }
+
+  useEffect(() => {
+    if (selectedItem === null && suggestItems.length > 0) {
+      setSelectedItem(0)
+    } else if (suggestItems.length === 0) {
+      setSelectedItem(null)
+    }
+  }, [suggestItems])
 
   return (
-    <div className='renderingArea'>
+    <div className='renderingArea' onKeyDown={handleKeyDown}>
       <div className='wrapper'>
         <div className={`formContainer ${isDirty ? 'isDirty' : ''}`}>
-          <input className='queryForm' type='text' onKeyDown={() => setIsDirty(true)} />
+          <input className='queryForm' type='text' onChange={onFormChange} />
         </div>
 
         {isDirty && (
           <div className='result'>
             <div className='suggestContainer'>
               <ul className='suggestList'>
-                <li className='suggestList-item'>SpringBoot @Transactional</li>
-                <li className='suggestList-item'>SpringBoot Mybatisのテスト</li>
-                <li className='suggestList-item'>SpringBoot Thymeleafのテスト</li>
-                <li className='suggestList-item'>WHERE句でnullを指定する</li>
-                <li className='suggestList-item'>hoge</li>
-                <li className='suggestList-item'>huga</li>
+                {sampleSuggestItems.map((item, index) => (
+                  <li
+                    key={`suggest-${index}`}
+                    className={`suggestList-item ${selectedItem === index ? 'selected' : ''}`}
+                  >
+                    {item}
+                  </li>
+                ))}
               </ul>
             </div>
 
