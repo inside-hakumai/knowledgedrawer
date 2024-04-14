477c8081336f6d125d18b29b984cfc345b898070
inside-hakumai <inside.rice164@gmail.com>
2024-04-14 14:51:32 +0900

    Vue導入

---
+++ src/renderer/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <p>hoge</p>
+    <p>count: {{ count }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<style lang="scss"></style>
